70e1b2061e89078a5f923350e09b207174ac10c6
George Trujillo <georgeu2021@gmail.com>
2025-03-24 14:44:57 -0500

    feat: add update wins for skins

--- Sources/App/Controllers/SkinSeasonController.swift
+++ Sources/App/Controllers/SkinSeasonController.swift
@@ -6,6 +6,7 @@ struct SkinSeasonController: RouteCollection {
         let skinSeasonRoutes = routes.grouped("skinseasons")
         skinSeasonRoutes.post(use: create)
         skinSeasonRoutes.get(use: index)
+        skinSeasonRoutes.patch("update-wins", use: updateWins)
     }
     
     func index(req: Request) async throws -> [SkinSeason] {
@@ -21,6 +22,34 @@ struct SkinSeasonController: RouteCollection {
             wins: skinSeasonRequest.wins,
             seasonID: skinSeasonRequest.seasonID
         )
+        
+        try await skinSeason.save(on: req.db)
+        return skinSeason
+    }
+    
+    func updateWins(req: Request) async throws -> SkinSeason {
+        
+        struct updateWinsRequest: Content {
+            var id: UUID
+            var wins: Int
+        }
+        
+        let updateRequest = try req.content.decode(updateWinsRequest.self)
+        
+        guard let skinSeason = try await SkinSeason.query(on: req.db)
+            .filter(\.$id == updateRequest.id)
+            .first() else {
+            throw Abort(.notFound, reason: "SkinSeason not found")
+        }
+        
+        skinSeason.wins = updateRequest.wins
+        
+        if let existSkin = try await Skin.query(on: req.db)
+            .filter(\.$name == skinSeason.name)
+            .first() {
+            existSkin.totalWins = updateRequest.wins
+            try await existSkin.save(on: req.db)
+        }
         
         try await skinSeason.save(on: req.db)
         return skinSeason
